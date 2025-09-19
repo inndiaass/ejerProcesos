1811afdbae66fde6a46a7eadab75ff4788366746
inndiaass <indiascr7@gmail.com>
2025-09-19 12:05:24 +0200

    ultimo comit (no funciona bien ahora la clase lanzar proceso)

--- lanzarProceso/src/lanzarProceso.java
+++ lanzarProceso/src/lanzarProceso.java
@@ -6,34 +6,33 @@ public class lanzarProceso {
         Scanner sc = new Scanner(System.in);
         String ruta = "C:\\Users\\oscar\\IdeaProjects\\ejer1Procesos\\out\\artifacts\\ejer1Procesos_jar\\ejer1Procesos.jar";
 
-        System.out.println("Introduce el primer número:");
-        int numero1 = sc.nextInt();
-        System.out.println("Introduce el segundo número:");
-        int numero2 = sc.nextInt();
-        sc.nextLine();
-        System.out.println("Introduce el operando:");
-        String operando = sc.nextLine();
+
 
         try {
+
             ProcessBuilder pb = new ProcessBuilder(
                     "java", "-jar", ruta
             );
 
             Process proceso = pb.start();
 
-            try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(proceso.getOutputStream()))) {
-                bw.write(numero1 + "\n");
-                bw.write(numero2 + "\n");
-                bw.write(operando + "\n");
-
-            }
-
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(proceso.getInputStream()))) {
+            try(BufferedReader br = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(proceso.getOutputStream())))  {
                 String linea;
                 while ((linea = br.readLine()) != null) {
-                    System.out.println("Salida del proceso: " + linea);
+                    System.out.println(linea);
+
+                    bw.write(sc.nextLine());
+                    bw.write("/n");
+                    bw.flush();
+
+                    System.out.println(br.readLine());
+                    bw.write(sc.nextLine());
+                    bw.write("/n");
+                    bw.flush();
                 }
             }
+
 
 
 
