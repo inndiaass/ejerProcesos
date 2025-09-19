7a2451308beabe43c3c22b47acc4216235ae6755
inndiaass <indiascr7@gmail.com>
2025-09-19 10:57:45 +0200

    calculadora ya hecha

---
+++ src/calculadora.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+    public class calculadora {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("introduce el primer numero");
+            int num1 = sc.nextInt();
+            System.out.println("introduce el segundo numero");
+            int num2 = sc.nextInt();
+            sc.nextLine();
+            System.out.println("introduce el operando");
+            String operando = sc.nextLine();
+            int res=0;
+
+
+            switch(operando){
+                case "+" -> res = num1 + num2;
+                case "-" -> res = num1 - num2;
+                case "/" -> res = num1 / num2;
+                case "%" -> res = num1 % num2;
+
+            }
+            System.out.println("el resultado es "+res);
+
+        }
+    }
+
